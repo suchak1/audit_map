19499c9dfb42fcc77405cac4a243f8e375f2262a
Krish Suchak <suchak.krish@gmail.com>
2019-09-08 03:20:28 -0400

    zoom works

--- src/Map.js
+++ src/Map.js
@@ -1,61 +1,28 @@
 import React, {Component} from 'react';
-import ReactMapGL, {Marker, NavigationControl} from 'react-map-gl';
-import logo from './logo.svg';
-import './Map.css';
+import ReactMapGL, {NavigationControl} from 'react-map-gl';
 
 class Map extends Component {
   state = {
     viewport: {
-    },
+      zoom: 1
+    }
   };
 
-  onViewportChange = viewport => {
-    const {width, height, ...etc} = viewport
-    this.setState({viewport: etc})
-  }
-
   render() {
-    const {
-      viewport,
-    } = this.state
+    const {viewport} = this.state;
     return (
-        <ReactMapGL {...viewport}
-          width='100vw'
-          height='75vh'
-          zoom={1}
-          mapStyle='mapbox://styles/suchak1/ck0ahex6o26ri1co8gmm3ezvr'
-          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
-          onViewportChange={viewport => this.onViewportChange(viewport)}>
-          <div style={{position: 'absolute', right: 0}}>
-            <NavigationControl />
-          </div>
-        </ReactMapGL>
+      <ReactMapGL {...viewport}
+        width = "100vw"
+        height = "75vh"
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+        mapStyle = 'mapbox://styles/suchak1/ck0ahex6o26ri1co8gmm3ezvr'
+        onViewportChange={viewport => this.setState({viewport})}>
+        <div style={{position: 'absolute', right: 0}}>
+          <NavigationControl />
+        </div>
+      </ReactMapGL>
     );
   }
 }
-// function App() {
-//   const virtru = process.env.REACT_APP_VIRTRU;
-//   const mapbox = process.env.REACT_APP_MAPBOX;
-//   console.log(virtru);
-//   console.log(mapbox)
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default Map;
