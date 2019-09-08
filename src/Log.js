daed3bbec0692133959190cf64de4c4b233de49b
Krish Suchak <suchak.krish@gmail.com>
2019-09-08 05:00:55 -0400

    new files

---
+++ src/Log.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './Log.css';
+// console.log(styles.log);
+
+class Log extends Component {
+  state = {
+  };
+
+  render() {
+    return (
+        <div className="log">
+          Audit Map
+        </div>
+    );
+  }
+}
+
+export default Log;
