91a4dc8d31a2b2d55aa6372331fabac888d3d114
Krish Suchak <suchak.krish@gmail.com>
2019-09-08 23:32:14 -0400

    font coloring for header

--- src/Log.js
+++ src/Log.js
@@ -2,6 +2,13 @@ import React, {Component} from 'react';
 import './Log.css';
 // console.log(styles.log);
 
+const lines = [
+  "POLICY.ACCESSED => POLICY.REVOKED",
+  "POLICY.OPENED => POLICY.REVOKED",
+  "POLICY.READ_ACCESS => POLICY.REVOKED",
+  "POLICY.WRITE_ACCESS => POLICY.REVOKED",
+  "POLICY.FETCH => POLICY.REVOKED"]
+
 class Log extends Component {
   state = {
   };
@@ -13,8 +20,8 @@ class Log extends Component {
             <span style={{color: "#4481F8"}}>
               <b>Audit Map</b>
             </span>
-            <span> | </span>
-            <span>
+            <span style={{color: "#343332"}}> | </span>
+            <span style={{fontSize: "0.85em", color: "dimgray"}}>
               an entry for the Virtru Privacy Engineering Challenge
             </span>
           </div>
