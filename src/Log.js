0e666a5c75063537c66ee3648c6d0d2ea634a77b
Krish Suchak <suchak.krish@gmail.com>
2019-09-10 10:01:14 -0400

    more progress

--- src/Log.js
+++ src/Log.js
@@ -17,36 +17,50 @@ class Log extends Component {
     );
   };
 
+  binary = (str) => {
+    let spans = [];
+    for(let i = 0; i < str.length; i++)
+    {
+      const randColor = Math.random()>0.3 ? '#4481F8' : 'dimgray';
+      spans.push(
+        <span style={{fontWeight:"bold", color: randColor}}>
+          {str[i]}
+        </span>);
+    }
+    return spans;
+  };
+
+
   render() {
     return (
         <div className="log">
           <div className="header">
-            <span style={{color: "#4481F8"}}>
-              <b>{"A̩̯͔̞̙̮̰͗͑͋̎̒̓̚ͅu̖͖̝̟͎̦̦̞͈̤̐̑̆̈̆̂̀̊͒̄̃͒ͅd̳͍̮̖͌̅́̋ì̗̣̥̜̯̫̰̰͛͌̉͐̔̇́̊̈́͂́t͙̘̭̫͐̏̃͑͊̌̾̔̚ͅ M̫̟̮̣͉̭̗̳̲͉̌̌̑͋̽͋̀͗͐̾̚̚ͅà͙͎̲̲̈́̄́͑̂̆̄p͎͉̣̱̳̗͉̬̋̀͒̓̽̿̊ͅ > "}</b>
+              <b>{this.binary("A̩̯͔̞̙̮̰͗͋̎̒̓̚ͅu̖͖̝̟͎̦̦̞͈̤̐̑̆̈̆̂̀̊͒̄̃͒ͅd̳͍̮̖͌̅́̋ì̗̣̥̜̯̫̰̰͛͌̉͐̔̇́̊̈́͂́t͙̘̭̫͐̏̃͑͊̌̾̔̚ͅ M̫̟̮̣͉̭̗̳̲͉̌̌̑͋̽͋̀͗͐̾̚̚ͅà͙͎̲̲̈́̄́͑̂̆̄p͎͉̣̱̳̗͉̬̋̀͒̓̽̿̊ͅ > ")}</b>
+            <span className="virtru">
+              >
             </span>
-            <span style={{fontSize: "0.7em", color: "dimgray"}}>
+            <span style={{fontSize: "0.8em", color: "dimgray"}}>
               &nbsp;&nbsp;an entry for the Virtru Privacy Engineering Challenge
             </span>
           </div>
-          <div >
-            <div style={{textAlign:"center", fontFamily: 'Ubuntu Mono'}}>
-              {"<Logs>"} <span className="waiting"> </span>
-            </div>
+          <div style={{textAlign:"center", fontFamily: 'Ubuntu Mono'}}>
+            {"Logs_"}
           </div>
-        <div className="list">
-        <AutoSizer>
-          {({height, width}) => (
-            <List
-              rowCount={this.props.data.length}
-              width={width}
-              height={height}
-              rowHeight={rowHeight}
-              rowRenderer={this.rowRenderer}
-              overscanRowCount={3}
-            />
-          )}
-        </AutoSizer>
-        </div>
+          <span className="waiting"> </span>
+          <div className="list">
+            <AutoSizer>
+              {({height, width}) => (
+                <List
+                  rowCount={this.props.data.length}
+                  width={width}
+                  height={height}
+                  rowHeight={rowHeight}
+                  rowRenderer={this.rowRenderer}
+                  overscanRowCount={3}
+                />
+              )}
+            </AutoSizer>
+          </div>
         </div>
     );
   }
