4e1bf1cd6c68c050d2d911c0309f210de9bdb266
Krish Suchak <suchak.krish@gmail.com>
2019-09-13 23:38:24 -0400

    file upload experiment

--- src/ControlPanel.js
+++ src/ControlPanel.js
@@ -9,25 +9,10 @@ export default class ControlPanel extends PureComponent {
 
     return (
       <Container>
-        <h3>Marker, Popup, NavigationControl and FullscreenControl </h3>
-        <p>
-          Map showing top 20 most populated cities of the United States. Click on a marker to learn
-          more.
-        </p>
-        <p>
-          Data source:{' '}
-          <a href="https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population">
-            Wikipedia
-          </a>
-        </p>
-        <div className="source-link">
-          <a
-            href="https://github.com/uber/react-map-gl/tree/5.0-release/examples/controls"
-            target="_new"
-          >
-            View Code ↗
-          </a>
-        </div>
+        <label>
+          <h4><button>Import File ↗ </button></h4>
+          <input type="file"></input>
+        </label>
       </Container>
     );
   }
