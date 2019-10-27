731f06ff3aace92ad1b22b5ad5b17d3ac41530a9
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2019-10-27 16:43:11 -0500

    Feature: Connect File Encryption to State (#13)
    * constructing new entry for state
    
    * done connecting encryption to state
    
    * version
    
    * ip-api to ipapi
    
    * ip2geo not working
    
    * done
    
    * log field
    
    * eslint error

--- src/ControlPanel.js
+++ src/ControlPanel.js
@@ -1,89 +1,97 @@
 import React, {PureComponent} from 'react';
 import Virtru from 'virtru-sdk';
+import publicIp from 'public-ip';
 import './ControlPanel.css';
 
 
 const defaultContainer = ({children}) => <div className="control-panel">{children}</div>;
 
 export default class ControlPanel extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      emails: [],
-      file: null
+    constructor(props) {
+        super(props);
+        this.state = {
+            emails: [],
+        }
     }
-  }
 
-  setEmails = (emails) => {
-    const addresses = emails.split(",").map(email => email.trim());
-    this.setState({emails: addresses});
-  }
+    setEmails = (emails) => {
+        const addresses = emails.split(",").map(email => email.trim());
+        this.setState({emails: addresses});
+    }
 
 
-  handleClick = () => {console.log(this.state)};
+    render() {
+        const Container = this.props.containerComponent || defaultContainer;
+        const encryptRef = React.createRef();
 
-  render() {
-    const Container = this.props.containerComponent || defaultContainer;
-    const encryptRef = React.createRef();
-    const emailRef = React.createRef();
+        return (
+            <Container>
+                <span>
+                    <div className="field">
+                        <input fontFamily='Maven Pro' type="text" id="name" name="name"
+                            required size="50" onChange={(e) => {this.setEmails(e.target.value)}}
+                            placeholder="Enter emails with whom to share file (sep. by commas)"/>
+                    </div>
+                    <div className="upload-encrypt-wrapper">
+                        <button className="encrypt">Encrypt File ↗</button>
+                        <input
+                            id="file_input_file"
+                            className="none"
+                            type="file"
+                            ref={encryptRef}
+                            onChange={(e) => {
+                                if(encryptRef.current.files.length) {
+                                    const fileName = encryptRef.current.files[0].name;
+                                    const reader = new FileReader();
+                                    reader.onload = () => {
+                                        const file = reader.result;
 
-    return (
-      <Container>
-        <span>
-        <div className="field">
-          <input fontFamily='Maven Pro' type="text" id="name" name="name" ref={emailRef}
-            required size="50" onChange={(e) => {this.setEmails(e.target.value)}}
-            placeholder="Enter emails with whom to share file (sep. by commas)"/>
-        </div>
-        <div className="upload-encrypt-wrapper">
-            <button className="encrypt" onClick={this.handleClick()}>Encrypt File ↗</button>
-              <input
-              id="file_input_file"
-              className="none"
-              type="file"
-              ref={encryptRef}
-              onChange={(e) => {
-                if(encryptRef.current.files.length) {
-                  const reader = new FileReader();
-                  reader.onload = () => {
-                    const file = reader.result;
-                    console.log(file.byteLength);
-                    console.log(file);
-                    const virtru = async () => {
-                      const email = process.env.REACT_APP_EMAIL;
-                      if(!Virtru.Auth.isLoggedIn({email: email})) {
-                        Virtru.Auth.loginWithGoogle({email: email, redirectUrl: "http://localhost:3000/"});
-                      }
-                      const policy = new Virtru.PolicyBuilder().build();
-                      const client = new Virtru.Client({email});
-                      console.log(this.state.emails);
-                      const encryptParams = new Virtru.EncryptParamsBuilder()
-                      .withArrayBufferSource(file)
-                      .withUsersWithAccess(this.state.emails)
-                      .withPolicy(policy)
-                      .build();
+                                        const virtru = async () => {
+                                            const email = process.env.REACT_APP_EMAIL;
+                                            if(!Virtru.Auth.isLoggedIn({email: email})) {
+                                                Virtru.Auth.loginWithGoogle({email: email, redirectUrl: "http://localhost:3000/"});
+                                            }
+                                            const policy = new Virtru.PolicyBuilder().build();
+                                            const client = new Virtru.Client({email});
+                                            console.log(this.state.emails);
+                                            const encryptParams = new Virtru.EncryptParamsBuilder()
+                                            .withArrayBufferSource(file)
+                                            .withUsersWithAccess(this.state.emails)
+                                            .withPolicy(policy)
+                                            .build();
 
-                      const ct = await client.encrypt(encryptParams);
-                      await ct.toFile('encrypted.html');
-                    }
-                    virtru();
-                    this.setState({file: file})
-                  }
-                  let file = encryptRef.current.files[0];
-                  this.setState({file: file.name});
-                  reader.readAsArrayBuffer(file);
-                  console.log(file);
-                  //this.setState({file: file});
-                  // require user to store file in directory
-                  // only record filename, pass filename to node.js file + e
-                  // write json file after encrypting file - first level is encrypt paramas index then ip, users, filename, etc
-                  // data structure in react app should be list of dicts encrypt params
-                }
-              }}
-            />
-        </div>
-        </span>
-      </Container>
-    );
-  }
+                                            const ct = await client.encrypt(encryptParams);
+                                            await ct.toFile('encrypted-' + fileName + '.html');
+
+                                            const policyId = encryptParams.getPolicyId();
+                                            console.log(policyId);
+                                            const entry = {
+                                                [policyId]: {
+                                                    users: this.state.emails,
+                                                    file: fileName,
+                                                    access: 'GRANT',
+                                                    ip: await publicIp.v4()
+                                                }
+                                            };
+                                            console.log(entry);
+                                            this.props.addPolicy(policyId, entry[policyId]);
+
+                                        }
+                                        virtru();
+                                    }
+                                    let file = encryptRef.current.files[0];
+                                    reader.readAsArrayBuffer(file);
+
+                                    // require user to store file in directory
+                                    // only record filename, pass filename to node.js file + e
+                                    // write json file after encrypting file - first level is encrypt paramas index then ip, users, filename, etc
+                                    // data structure in react app should be list of dicts encrypt params
+                                }
+                            }}
+                            />
+                    </div>
+                </span>
+            </Container>
+        );
+    }
 }
